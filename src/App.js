3641bc021ccaf52661d68fa41042a8c771733976
Leandro Faria <leandro@leandrofaria.com>
2021-09-28 10:50:54 -0300

    Responsive side menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
+import { useState } from 'react';
 import PrimeReact from 'primereact/api';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './Routing';
@@ -10,12 +11,22 @@ import SideMenu from './Componentes/SideMenu';
 function App() {
 	PrimeReact.ripple = true;
 
+	const toggleSideMenu = () => {
+		setShowSideMenu(!showSideMenu);
+	};
+
+	const closeMenus = () => {
+		setShowSideMenu(false);
+	};
+
+	const [showSideMenu, setShowSideMenu] = useState(true);
+
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<NavBar />
+				<NavBar toggleSideMenu={toggleSideMenu} />
 				<div id="mainContainer">
-					<SideMenu />
+					<SideMenu showSideMenu={showSideMenu} closeMenus={closeMenus} />
 					<div id="contentContainer">
 						<Routing />
 					</div>
