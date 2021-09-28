712d12ff126cbb605a4d1c2e5ea37f0c2eff1669
Leandro Faria <leandro@leandrofaria.com>
2021-09-28 09:39:44 -0300

    Mudança do pacote de ícones

--- src/Componentes/SideMenu.jsx
+++ src/Componentes/SideMenu.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import { FaInfoCircle } from 'react-icons/fa';
+import { AiFillWarning } from 'react-icons/ai';
 
 function SideMenu() {
 	return (
 		<div id="sideMenu">
-			<p>Menu Lateral</p>
-			<hr />
-			<Link to="about">Sobre</Link>
+			<ul>
+				<li>
+					<Link to="/">
+						<FaHome />
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link to="/about">
+						<FaInfoCircle />
+						Sobre
+					</Link>
+				</li>
+				<li>
+					<Link to="/notfound">
+						<AiFillWarning />
+						Not Found
+					</Link>
+				</li>
+			</ul>
 		</div>
 	);
 }
